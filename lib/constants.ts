dc5a0d3acf56012e061a2500a92b3538ea33cfb4
Ali Mousavi <amousavig@icloud.com>
2024-10-06 02:27:59 +0330

    Added contract ABI.

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,3 +16,110 @@ export const network = {
 		currencySymbol: 'GAS',
 	},
 };
+
+export const contractABI = [
+	{
+		anonymous: false,
+		inputs: [
+			{
+				indexed: true,
+				internalType: 'address',
+				name: 'user',
+				type: 'address',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'username',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'bio',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'string',
+				name: 'avatarHash',
+				type: 'string',
+			},
+			{
+				indexed: false,
+				internalType: 'string[]',
+				name: 'socialLinks',
+				type: 'string[]',
+			},
+		],
+		name: 'ProfileUpserted',
+		type: 'event',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: '_user',
+				type: 'address',
+			},
+		],
+		name: 'getProfile',
+		outputs: [
+			{
+				internalType: 'string',
+				name: 'username',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'bio',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: 'avatarHash',
+				type: 'string',
+			},
+			{
+				internalType: 'string[]',
+				name: 'socialLinks',
+				type: 'string[]',
+			},
+			{
+				internalType: 'bool',
+				name: 'exists',
+				type: 'bool',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'string',
+				name: '_username',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: '_bio',
+				type: 'string',
+			},
+			{
+				internalType: 'string',
+				name: '_avatarHash',
+				type: 'string',
+			},
+			{
+				internalType: 'string[]',
+				name: '_socialLinks',
+				type: 'string[]',
+			},
+		],
+		name: 'upsertProfile',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+];
