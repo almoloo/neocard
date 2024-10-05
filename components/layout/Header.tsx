361da71d2b363f096bddcfe9de873def31e7590c
Ali Mousavi <amousavig@icloud.com>
2024-10-06 02:56:11 +0330

    Updated header UI.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
 import ConnectButton from './ConnectButton';
+import { Afacad } from 'next/font/google';
+import { UsersIcon } from 'lucide-react';
+
+const afacad = Afacad({
+	subsets: ['latin'],
+	weight: ['700'],
+});
 
 export default function Header() {
 	return (
-		<div>
-			Header
+		<header className="flex items-center justify-between bg-white px-5 py-3 border-b">
+			<Link href="/">
+				<strong
+					className={`${afacad.className} flex items-center gap-2 text-2xl`}
+				>
+					<span className="bg-rose-400 p-2 rounded-full">
+						<UsersIcon className="w-4 h-4 text-rose-900" />
+					</span>
+					<span>NeoCard</span>
+				</strong>
+			</Link>
 			<ConnectButton />
-		</div>
+		</header>
 	);
 }
