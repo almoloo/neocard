4f5c96576539ed84e8f161c83771c90fc56852de
Ali Mousavi <amousavig@icloud.com>
2024-10-06 02:27:50 +0330

    Added convertIPFSHashToURL function.

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function isTestnet() {
 	return process.env.NEXT_PUBLIC_NETWORK === 'testnet';
 }
+
+export function convertIPFSHashToURL(hash: string) {
+	return `${process.env.NEXT_PUBLIC_PINATA_GATEWAY}/ipfs/${hash}?pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_GATEWAY_KEY}`;
+}
